8e55aebc5adc5643f40ad5d14f3bcdb4cfb04139
shivamrajpoot7070 <shivamrajpoot7070@gmail.com>
2025-01-14 23:12:32 +0530

    fixing db

--- backend/utils/dbconnect.js
+++ backend/utils/dbconnect.js
@@ -1,3 +1,5 @@
+import mongoose, { connect } from "mongoose";
+
 const connectDB = async () => {
     try {
       await mongoose.connect(process.env.MONGO_URI, {
@@ -9,4 +11,5 @@ const connectDB = async () => {
       console.error("MongoDB connection error:", e.message);
     }
   };
-  
+
+export default connectDB;
