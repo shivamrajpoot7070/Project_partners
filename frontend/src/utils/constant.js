c588f2abc4158dda4f03b03abd468d6e93ecb93a
shivamrajpoot7070 <shivamrajpoot7070@gmail.com>
2025-01-14 23:35:48 +0530

    changing bckend url

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const USER_END_POINT="http://localhost:1000/api/v1/user";
-export const JOB_END_POINT="http://localhost:1000/api/v1/job";
-export const APPLICATION_END_POINT="http://localhost:1000/api/v1/application";
-export const COMPANY_END_POINT="http://localhost:1000/api/v1/company";
+export const USER_END_POINT="https://project-partners-amber.vercel.app/api/v1/user";
+export const JOB_END_POINT="https://project-partners-amber.vercel.app/api/v1/job";
+export const APPLICATION_END_POINT="https://project-partners-amber.vercel.app/api/v1/application";
+export const COMPANY_END_POINT="https://project-partners-amber.vercel.app/api/v1/company";
