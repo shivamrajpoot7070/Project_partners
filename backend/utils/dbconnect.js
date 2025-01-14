2bb8168f2fb6d1ded2e4c6e031db641e9f773fe3
shivamrajpoot7070 <shivamrajpoot7070@gmail.com>
2025-01-14 23:10:39 +0530

    fixing db

--- backend/utils/dbconnect.js
+++ backend/utils/dbconnect.js
@@ -1,15 +1,12 @@
-import mongoose, { connect } from "mongoose";
-
-const connectDB=async()=>{
-    try{
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log("mongodb connected successfully");
+const connectDB = async () => {
+    try {
+      await mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      console.log("MongoDB connected successfully");
+    } catch (e) {
+      console.error("MongoDB connection error:", e.message);
     }
-
-    catch(e){
-        console.log(e)
-    }
-
-}
-
-export default connectDB;
+  };
+  
